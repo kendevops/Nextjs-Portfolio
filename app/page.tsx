7a8154a231d7d85871d74ce20cfdf364ecbc4156
Kenneth Aladi <kendevops@gmail.com>
2024-05-08 22:55:21 +0100

    completed the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="w-full h-full">
-      <div className="flex flex-col gap-20 h-[850px]">
+      <div className="flex flex-col gap-20">
         <Hero />
       </div>
     </main>
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import React from "react";
 import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 const HeroContent = () => {
   return (
@@ -9,8 +18,52 @@ const HeroContent = () => {
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-        <motion.div className="Welcome-box py-[15px] px-[4px] border-[#7042f88b] opacity-[0.9]"></motion.div>
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Senior Frontend Developer Portfolio
+          </h1>
+        </motion.div>
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> the best </span>
+            project experience
+          </span>
+        </motion.div>
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I am a Senior Frontend Engineer committed to contributing to the
+          success of projects and exceeding client expectations. I have honed my
+          skills in various frontend technologies, delivering high-quality web
+          applications. Check out my projects and skills.
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
       </div>
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="skills icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
     </motion.div>
   );
 };
