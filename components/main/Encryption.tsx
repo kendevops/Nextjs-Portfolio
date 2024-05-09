772eae399b0e5fc24d655264d6b58d9ffa5260e3
Kenneth Aladi <kendevops@gmail.com>
2024-05-09 02:44:10 +0100

    added more text on encryption and fixed design issues

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 const Encryption = () => {
   return (
-    <div className="flex flex-row relative items-center justify-center w-full h-full">
+    <div className="flex flex-row relative items-center justify-center w-full h-full min-h-screen">
       <div className="absolute w-auto h-auto top-0 z-[5]">
         <motion.div
           variants={slideInFromTop}
@@ -37,6 +37,16 @@ const Encryption = () => {
             height={70}
             className="z-10"
           />
+        </div>
+      </div>
+
+      <div className="Welcome-box px-[15px] py-[4px] z-[20] border my-[20px] border-[#7042f88b] opacity=[0.9]">
+        <h1 className="Welcome-text text-[12px]">Encryption</h1>
+      </div>
+
+      <div className="absolute z-[20] bottom-[10px] px-[5px]">
+        <div className="cursive text-[20px] font-medium text-center text-gray-300">
+          Secure your data with end to end encryption
         </div>
       </div>
 
