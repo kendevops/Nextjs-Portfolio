47a1384198470372eb4f558c54b7465e2c8e33d4
Kenneth Aladi <kendevops@gmail.com>
2024-05-10 06:46:56 +0100

    created the contact me section

---
+++ components/main/Contact.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ContactForm from "../sub/ContactForm";
+
+const Contact = () => {
+  return (
+    <div
+      id="contact"
+      className="w-screen h-screen bg-cover flex items-start justify-center"
+    >
+      <div
+        style={{ background: "url(atombg-comp.webp)" }}
+        className="h-[60%] w-[80%] relative bg-cover bg-center rounded-xl border border-white"
+      >
+        <div className="absolute left-20 bottom-16 w-[70%] md:w-[30%] ">
+          <ContactForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ components/sub/ContactForm.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <form>
+      <h2 className="text-2xl font-bold mb-5 text-white">Contact Me</h2>
+      <div className="mb-3">
+        <input
+          type="text"
+          placeholder="Your Name"
+          name="name"
+          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+        />
+      </div>
+      <div className="mb-3">
+        <input
+          type="email"
+          placeholder="Email"
+          name="name"
+          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+        />
+      </div>
+      <div className="mb-3">
+        <textarea
+          placeholder="Your Message"
+          name="message"
+          className="w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow"
+        />
+      </div>
+      <button className="px-6 mb-5 py-3 text-sm font-bold rounded-md text-white uppercase transition-all duration-150 ease-linear bg-blue-500 hover:blue-400 cursor-pointer">
+        Send Message
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
