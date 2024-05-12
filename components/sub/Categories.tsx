9985983944136235ddfa60cfbce92e9f85d903f8
Kenneth Aladi <kendevops@gmail.com>
2024-05-12 13:24:50 +0100

    padding on button

--- components/sub/Categories.tsx
+++ components/sub/Categories.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 function Categories({ filter, categories }: Props) {
-  const [activeIndex, setActiveIndex] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const handleClick = (
     category: string,
@@ -20,11 +20,11 @@ function Categories({ filter, categories }: Props) {
 
   return (
     <div className="w-[500px] h-full flex flex-row items-center justify-center my-20">
-      <div className="flex items-center justify-between w-full h-auto border-[#7042f861] bg-purple-800 mr-[10px] px-[40px] py-[10px] rounded-full">
+      <div className="flex items-center justify-between w-full h-auto border-[#7042f861] bg-purple-800 mr-[10px] py-[10px] rounded-full">
         {categories.map((category, i) => (
           <button
             type="button"
-            className={`cursor-pointer ${
+            className={`px-[40px] ${
               activeIndex === i ? "text-black bg-white" : "text-gray-300"
             }`} // Dynamically add active class
             key={i}
